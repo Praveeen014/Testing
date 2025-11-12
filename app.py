17de1f13c8603fb945ef83e24698526b38752660
Praveen <155132070+Praveeen014@users.noreply.github.com>
2025-11-12 16:14:27 +0545

    something

--- app.py
+++ app.py
@@ -1,31 +1,26 @@
-# import requests
-# from parsel import Selector
+import requests
+from parsel import Selector
 
 
-# for i in range(1,3):
-#     r=requests.get(f'https://books.toscrape.com/catalogue/page-{i}.html')
+for i in range(1,3):
+    r=requests.get(f'https://books.toscrape.com/catalogue/page-{i}.html')
 
-#     response=Selector(r.text)
-#     for list in response.xpath('//li/article'):
-#       title=list.xpath('.//h3/a/text()').get()
+    response=Selector(r.text)
+    for list in response.xpath('//li/article'):
+      title=list.xpath('.//h3/a/text()').get()
      
-#       img_url=list.xpath('.//img/@src').get()
-#       url = "https://books.toscrape.com/" +img_url 
+      img_url=list.xpath('.//img/@src').get()
+      url = "https://books.toscrape.com/" +img_url 
       
-#       price=list.xpath('.//p[@class="price_color"]/text()').get()
+      price=list.xpath('.//p[@class="price_color"]/text()').get()
       
-#       rating=list.xpath('.//p/@class').get().split(" ")[1]
+      rating=list.xpath('.//p/@class').get().split(" ")[1]
 
-#       avaialble=list.xpath('.//p[@class="instock availability"]/text()').getall()[1].strip()
+      avaialble=list.xpath('.//p[@class="instock availability"]/text()').getall()[1].strip()
 
-#       print(f"TITLE: {title}")
-#       print(f"PRICE: {price}")
-#       print(f"RATING: {rating}")
-#       print(f"URL: {url}")
-#       print(f"AVAILABLE: {avaialble}")
-#       print(" ")
-
-
-
-
-print("hello world")
+      print(f"TITLE: {title}")
+      print(f"PRICE: {price}")
+      print(f"RATING: {rating}")
+      print(f"URL: {url}")
+      print(f"AVAILABLE: {avaialble}")
+      print(" ")
